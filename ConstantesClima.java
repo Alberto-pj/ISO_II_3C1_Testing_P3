510cbe44c8443f99bd628ab48b21756228389e45
David Dionisio <David.Dionisio@alu.uclm.es>
2025-12-06 00:55:48 +0100

    Clase auxiliar constantes

---
+++ ConstantesClima.java
@@ -0,0 +1,11 @@
+/**
+ * Clase auxiliar para definir los tipos de precipitación.
+ * Usamos 'public static final' para crear constantes accesibles desde fuera.
+ */
+
+public class ConstantesClima {
+    // Asignamos un número a cada tipo para identificarlos
+    public static final int PRECIPITACION_NINGUNA = 0;
+    public static final int PRECIPITACION_AGUA = 1;  // Lluvia
+    public static final int PRECIPITACION_NIEVE = 2;
+}
