6369d7c8730af44d47b4fe1ea5f4614d8c1b9917
coco <jorge.assis@alu.uclm.es>
2025-12-18 19:27:10 +0100

    Cobertura de App.java

--- testing_p3/src/test/java/com/example/AppTest.java
+++ testing_p3/src/test/java/com/example/AppTest.java
@@ -1,11 +1,11 @@
-package testing_p3.src.test.java.com.example;
+package testing_p3.src.main.java.com.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 class AppTest {
 
     @Test
-    void ejemplo() {
-        assertTrue(true);
+    void main_noLanzaExcepcion() {
+        assertDoesNotThrow(() -> App.main(new String[0]));
     }
 }
